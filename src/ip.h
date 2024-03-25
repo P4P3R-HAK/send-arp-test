af560e2615d5ad3638e3993ee3c48b17892cefb6
P4P3R-HAK <kbfanta@naver.com>
2024-03-25 12:22:03 -0400

    send-arp-code

--- src/ip.h
+++ src/ip.h
@@ -4,34 +4,34 @@
 #include <string>
 
 struct Ip final {
-	static const int SIZE = 4;
+    static const int SIZE = 4;
 
-	// constructor
-	Ip() {}
-	Ip(const uint32_t r) : ip_(r) {}
-	Ip(const std::string r);
+    // constructor
+    Ip() {}
+    Ip(const uint32_t r) : ip_(r) {}
+    Ip(const std::string r);
 
-	// casting operator
-	operator uint32_t() const { return ip_; } // default
-	explicit operator std::string() const;
+    // casting operator
+    operator uint32_t() const { return ip_; } // default
+    explicit operator std::string() const;
 
-	// comparison operator
-	bool operator == (const Ip& r) const { return ip_ == r.ip_; }
+    // comparison operator
+    bool operator == (const Ip& r) const { return ip_ == r.ip_; }
 
-	bool isLocalHost() const { // 127.*.*.*
-		uint8_t prefix = (ip_ & 0xFF000000) >> 24;
-		return prefix == 0x7F;
-	}
+    bool isLocalHost() const { // 127.*.*.*
+        uint8_t prefix = (ip_ & 0xFF000000) >> 24;
+        return prefix == 0x7F;
+    }
 
-	bool isBroadcast() const { // 255.255.255.255
-		return ip_ == 0xFFFFFFFF;
-	}
+    bool isBroadcast() const { // 255.255.255.255
+        return ip_ == 0xFFFFFFFF;
+    }
 
-	bool isMulticast() const { // 224.0.0.0 ~ 239.255.255.255
-		uint8_t prefix = (ip_ & 0xFF000000) >> 24;
-		return prefix >= 0xE0 && prefix < 0xF0;
-	}
+    bool isMulticast() const { // 224.0.0.0 ~ 239.255.255.255
+        uint8_t prefix = (ip_ & 0xFF000000) >> 24;
+        return prefix >= 0xE0 && prefix < 0xF0;
+    }
 
 protected:
-	uint32_t ip_;
+    uint32_t ip_;
 };
